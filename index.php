0dde33ef8896c6c15eac2d3d4592a1a4a7c5d2a7
ashenalbum <1049019534@qq.com>
2020-09-24 18:41:59 +0800

    edit

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 $url = $_SERVER['QUERY_STRING'];
 
 switch($url){
-    case "":require "pages/album.html";break;
-    case "/index":require "pages/index.html";break;
+    case "":require "pages/three.html";break;
+    case "/index":require "pages/three.html";break;
     case "/album":require "pages/album.html";break;
     case "/album_set_provite":require "pages/album_set.html";break;
     case "/notes":require "pages/notes.html";break;
