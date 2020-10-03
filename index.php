1c9cbbed47fef47e85dff21df7eca3c795d04bff
ashenalubm <1049019534@qq.com>
2020-10-04 01:07:46 +0800

    e

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $url = $_SERVER['QUERY_STRING'];
 
 switch($url){
     case "":require "pages/album.html";break;
-    case "/index":require "pages/index.html";break;
+    case "/three":require "pages/three.html";break;
     case "/album":require "pages/album.html";break;
     case "/album_set_provite":require "pages/album_set.html";break;
     case "/notes":require "pages/notes.html";break;
