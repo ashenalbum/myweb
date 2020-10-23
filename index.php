0c5fc41306ddf1df6d1477a85f77f85a65e52d30
ashenalbum <1049019534@qq.com>
2020-10-23 11:10:31 +0800

    edit

--- index.php
+++ index.php
@@ -1,19 +1,14 @@
 ﻿<?php
-$url = $_SERVER['QUERY_STRING'];
-if($url==""){
-    require "pages/album.html";
-}else{
-    $base = preg_match("/^\/?[^?#/]+/",$url);
-    if(!$base){
-        require "src/404/404.html";
-    }else{
-        switch($base[0]){
-            case "/three":require "pages/three.html";break;
-            case "/album":require "pages/album.html";break;
-            case "/album_set_provite":require "pages/album_set.html";break;
-            case "/notes":require "pages/notes.html";break;
-            default:require "src/404/404.html";
-        }
-    }
+
+$query = $_SERVER['QUERY_STRING'];
+
+switch($query){
+    case "": require "pages/album.html"; break;
+    case "/three":require "pages/three.html";break;
+    case "/album":require "pages/album.html";break;
+    case "/album_set_provite":require "pages/album_set.html";break;
+    case "/notes":require "pages/notes.html";break;
+    default:require "src/404/404.html";
 }
+
 ?>
