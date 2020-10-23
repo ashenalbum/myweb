9eacb67452d5bdf02f39014d83457edeb7ca307e
ashenalbum <1049019534@qq.com>
2020-10-23 10:11:51 +0800

    page hash

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 ﻿<?php
 $url = $_SERVER['QUERY_STRING'];
-
-switch($url){
-    case "":require "pages/album.html";break;
-    case "/three":require "pages/three.html";break;
-    case "/album":require "pages/album.html";break;
-    case "/album_set_provite":require "pages/album_set.html";break;
-    case "/notes":require "pages/notes.html";break;
-    default:require "src/404/404.html";
+if($url==""){
+    require "pages/album.html";
+}else{
+    $base = preg_match("/^\/?[^?#/]+/",$url);
+    if(!$base){
+        require "src/404/404.html";
+    }else{
+        switch($base[0]){
+            case "/three":require "pages/three.html";break;
+            case "/album":require "pages/album.html";break;
+            case "/album_set_provite":require "pages/album_set.html";break;
+            case "/notes":require "pages/notes.html";break;
+            default:require "src/404/404.html";
+        }
+    }
 }
 ?>
